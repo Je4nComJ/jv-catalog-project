91d2e466aaa83163ef3b6eda30e732798d3b39f8
Je4nComJ <jeanvictor.fa@gmail.com>
2024-08-20 14:14:46 -0300

    Backend project created

---
+++ backend/src/main/java/com/gomesdevelopers/jvcatalog/JvcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.gomesdevelopers.jvcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gomesdevelopers/jvcatalog/JvcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomesdevelopers.jvcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
