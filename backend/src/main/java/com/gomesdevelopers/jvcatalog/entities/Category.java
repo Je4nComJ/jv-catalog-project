34db94dda2fdea8082cec9986c7aee1f8dc05a7d
Je4nComJ <jeanvictor.fa@gmail.com>
2024-08-26 21:24:37 -0300

    Product entity, SQL script

--- backend/src/main/java/com/gomesdevelopers/jvcatalog/entities/Category.java
+++ backend/src/main/java/com/gomesdevelopers/jvcatalog/entities/Category.java
@@ -1,8 +1,10 @@
 package com.gomesdevelopers.jvcatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +21,12 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 	
 	public Category() {
 	}
